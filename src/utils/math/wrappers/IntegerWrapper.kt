2b419341ea1f2bc182d7a0608d8a04631fce9ab0
DallogFheir <dallog.fheir@gmail.com>
2026-07-29 20:31:02 +0200

    refactor: move static methods to end of class

--- src/utils/math/wrappers/IntegerWrapper.kt
+++ src/utils/math/wrappers/IntegerWrapper.kt
@@ -4,12 +4,6 @@ import kotlin.math.abs
 import kotlin.math.pow
 
 open class IntegerWrapper(val number: Long) {
-    companion object {
-        fun isInteger(number: Double): Boolean {
-            return number % 1.0 == 0.0
-        }
-    }
-
     val length: Int = abs(number).toString().length
     val firstDigit: Int = (abs(number) / (10.0.pow(length - 1).toLong())).toInt()
 
@@ -35,5 +29,11 @@ open class IntegerWrapper(val number: Long) {
         val modulo = 10.0.pow(length - 1).toLong()
 
         return IntegerWrapper(number % modulo)
+    }
+
+    companion object {
+        fun isInteger(number: Double): Boolean {
+            return number % 1.0 == 0.0
+        }
     }
 }
