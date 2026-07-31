2b87bcc35c921bf96cc196cf7ad7fabe269f150e
DallogFheir <dallog.fheir@gmail.com>
2026-07-31 22:33:30 +0200

    feat: day 9 - part 1

---
+++ src/day9/Part1.kt
@@ -0,0 +1,36 @@
+package day9
+
+import utils.filereader.FileReader
+import utils.math.euclidean.Point
+import kotlin.math.abs
+
+private const val COORDINATE_SEPARATOR = ","
+private const val POINT_DIMENSIONALITY = 2
+
+
+object Part1 {
+    fun solve(dayNumber: Int, fileName: String): Long {
+       val points = FileReader(dayNumber = dayNumber, fileName = fileName).readLinesWithParser { line ->
+           val coordinateStrings = line.split(COORDINATE_SEPARATOR)
+
+           require(coordinateStrings.size == POINT_DIMENSIONALITY)
+
+           val coordinates = coordinateStrings.map { it.toDouble() }
+
+           Point(*coordinates.toDoubleArray())
+       }
+
+        val areas = points.flatMapIndexed { index, point ->
+            val otherPoints = points.slice(index + 1..points.lastIndex)
+
+            otherPoints.map {
+                val width = abs(point.coordinates[0] - it.coordinates[0]) + 1
+                val height = abs(point.coordinates[1] - it.coordinates[1]) + 1
+
+                width * height
+            }
+        }
+
+        return areas.max().toLong()
+    }
+}
---
+++ src/day9/Part2.kt
@@ -0,0 +1,7 @@
+package day9
+
+object Part2 {
+    fun solve(dayNumber: Int, fileName: String): Long {
+        TODO()
+    }
+}
